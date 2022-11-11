30201ec44c63b593bfc5f213d3f13fefb1cc93bd
Sihab-10 <sihabsarar10@gmail.com>
2022-11-11 10:37:52 +0600

    data modified

--- var-let-const.js
+++ var-let-const.js
@@ -1,6 +1,10 @@
+// difference between let and const
+
 let age = 11;
 age = 12;
 
 const countryName = "Bangladesh";
 // countryName = "Chad";
 console.log(age, countryName);
+
+// so let can be changeable but const doesn't change anymore
